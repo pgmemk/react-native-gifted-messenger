97efa3c6149467cfddfa77b43479a91cc69b048e
pgmemk <ellen.katsnelson@lablz.com>
2016-07-07 22:04:29 -0400

    multiline requires send button

--- GiftedMessenger.js
+++ GiftedMessenger.js
@@ -496,7 +496,6 @@ var GiftedMessenger = React.createClass({
             placeholder={this.props.placeholder}
             ref='textInput'
             onChangeText={this.onChangeText}
-            multiline={true}
             value={this.state.text}
             autoFocus={this.props.autoFocus}
             returnKeyType={this.props.submitOnReturn ? 'send' : 'default'}
